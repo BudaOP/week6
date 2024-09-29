d886129f5ab6d30baa20b579f4644a7558773534
BudaOP <ibudanov24@gmail.com>
2024-09-29 15:28:31 +0300

    week6 monday assignment. Coding-marathon and pair-programmings on different repos

--- friday/activity2/week6-be-workout-v2/index.js
+++
@@ -1,8 +0,0 @@
-const app = require("./app");
-const http = require("http");
-
-const server = http.createServer(app);
-
-server.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`);
-});
